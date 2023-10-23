0481a7abc5aca4c598adaf6b2d196dad33209628
pincky6 <mentovsky4@gmail.com>
2023-10-23 22:47:57 +0300

    add Client class

---
+++ Client.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Session.h"
+#include "Message.h"
+
+class Client
+{
+public:
+	Client(const std::string&, std::size_t);
+
+	void connect();
+	void dissconnect();
+
+	void send(const std::string&);
+	Message receive();
+
+	~Client();
+
+private:
+	asio::io_service ios;
+	asio::ip::tcp::endpoint endpoint;
+	Session session;
+};
+
