bdc1e5bbe959422f1536eab309f3a4f151c69d75
pincky6 <mentovsky4@gmail.com>
2023-11-06 17:28:46 +0300

    update Message class

--- Message.cpp
+++ Message.cpp
@@ -1,1 +1,57 @@
 #include "Message.h"
+
+Message::Message(): message_("")
+{}
+Message::Message(const std::string& message) : message_(message)
+{}
+Message::Message(const Message& message): message_(message.getMessage())
+{}
+Message::Message(Message&& message): message_(std::move(message.message_))
+{}
+
+Message& Message::operator=(const Message& message)
+{
+    if(this == std::addressof(message))
+        return *this;
+    message_ = message.message_;
+    return *this;
+}
+Message& Message::operator=(Message&& message)
+{
+    if (this == std::addressof(message))
+        return *this;
+    message_ = std::move(message.message_);
+    return *this;
+}
+
+const std::string& Message::getMessage() const
+{
+    return message_;
+}
+std::string Message::getMessageForSend() const
+{
+    return messageSizeToBinaryString(message_) + "\n" + message_;
+}
+
+std::string Message::messageSizeToBinaryString(const std::string& message)
+{
+    std::string sizeString;
+    sizeString.resize(sizeof(std::string::size_type) * 8);
+    std::size_t size = message.size();
+    for (auto it = sizeString.rbegin(), end = sizeString.rend(); it != end; it++)
+    {
+        *it = '0' + (size & 1);
+        size >>= 1;
+    }
+    return sizeString;
+}
+
+std::size_t Message::messageSizeFromBinaryString(const std::string& binaryString)
+{
+    std::size_t size = 0, i = 0;
+    for (auto it = binaryString.rbegin(), end = binaryString.rend(); it != end; ++it)
+    {
+        size += (*it - '0') << (i++);
+    }
+    return size;
+}
--- Message.h
+++ Message.h
@@ -4,11 +4,16 @@
 class Message
 {
 public:
+	Message();
 	Message(const std::string&);
-	const std::string& getMessage() const;
+	Message(const Message&);
+	Message(Message&&);
 
-	void reserve(const std::size_t);
-	const std::size_t size() const;
+	Message& operator=(const Message&);
+	Message& operator=(Message&&);
+
+	const std::string& getMessage() const;
+	std::string getMessageForSend() const;
 
 	static std::string messageSizeToBinaryString(const std::string&);
 	static std::size_t messageSizeFromBinaryString(const std::string&);
